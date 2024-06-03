ecf29758f2cd87c62fb219d4701c4d66459de5f1
胡文凡 <wenfan.hu@infaith.com.cn>
2024-06-03 16:31:31 +0800

    Initial

---
+++ research-spring-starter/src/test/java/org/example/researchspringstarter/ResearchSpringStarterApplicationTests.java
@@ -0,0 +1,13 @@
+//package org.example.researchspringstarter;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ResearchSpringStarterApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
